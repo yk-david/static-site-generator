ede7a53180be319ce014b0b4e8c75b60b3a79c3b
yk-david <k.yongkeon@gmail.com>
2026-01-31 21:19:44 +0900

    add TextNode to HTMLNode and test

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,5 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import *
 
 
 class TestTextNode(unittest.TestCase):
@@ -18,6 +18,24 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode('This is a link node', TextType.LINK, 'https://www.google.com') 
         self.assertIsNotNone(node)
         self.assertIsNotNone(node2)
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode('This is a text node', TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, 'This is a text node')
+    
+    def test_image(self):
+        node = TextNode('This is an image', TextType.IMAGE, 'https://www.coupang.com')
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.props, {'src': 'https://www.coupang.com', 'alt': 'This is an image'})
+
+    def test_link(self):
+        node = TextNode('some link here', TextType.LINK, 'https://www.coursera.org')
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.props, {'href': 'https://www.coursera.org'})
+
 
 if __name__ == '__main__':
     unittest.main()
