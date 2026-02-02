e73ab016bd9301ae1c8c4d0a271afd19b4feff85
yk-david <k.yongkeon@gmail.com>
2026-02-02 23:52:54 +0900

    working on split_nodes_delimiter

--- src/textnode.py
+++ src/textnode.py
@@ -43,3 +43,15 @@ def text_node_to_html_node(text_node):
             return LeafNode('img', '', {'src': text_node.url, 'alt': text_node.text})
         
         raise ValueError(f'invalid text type: {text_node.text_type}')
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):  
+    delimiters = {
+        '*': TextType.BOLD, 
+        '_': TextType.ITALIC, 
+        '`': TextType.CODE
+    }
+    
+    nodes = old_nodes.split(delimiter)
+    new_nodes = []
+    if old_nodes.text.startswith(delimiter) or old_nodes.text.endswith(delimiter):
