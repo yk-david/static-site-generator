182fbf48ed3b1367c9acb7d9151a39d790513cdf
yk-david <k.yongkeon@gmail.com>
2026-02-13 14:17:39 +0900

    text_to_textnodes function implemented

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -134,5 +134,25 @@ class TestSplitNodes(unittest.TestCase):
         ], new_nodes)
 
 
+# class TestTextToTextNodes(unittest.TestCase):
+#     def test_bold_italic_code_image_link(self):
+#         node = TextNode(
+#             'This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)', TextType.TEXT)
+#         new_nodes = text_to_textnodes(node)
+#         self.assertListEqual([
+#             TextNode("This is ", TextType.TEXT),
+#             TextNode("text", TextType.BOLD),
+#             TextNode(" with an ", TextType.TEXT),
+#             TextNode("italic", TextType.ITALIC),
+#             TextNode(" word and a ", TextType.TEXT),
+#             TextNode("code block", TextType.CODE),
+#             TextNode(" and an ", TextType.TEXT),
+#             TextNode("obi wan image", TextType.IMAGE,
+#                      "https://i.imgur.com/fJRm4Vk.jpeg"),
+#             TextNode(" and a ", TextType.TEXT),
+#             TextNode("link", TextType.LINK, "https://boot.dev"),
+#         ], new_nodes)
+
+
 if __name__ == '__main__':
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -115,3 +115,37 @@ def split_nodes_link(old_nodes):
                 new_nodes.append(TextNode(current_text, TextType.TEXT))
 
     return new_nodes
+
+
+def text_to_textnodes(text):
+    node = TextNode(text, TextType.TEXT)
+    new_nodes = split_nodes_image(split_nodes_link([node]))
+    for node in new_nodes:
+        if node.text_type == TextType.TEXT:
+            if '**' in node.text:
+                new_nodes = split_nodes_delimiter(
+                    new_nodes, '**', TextType.BOLD)
+            if '_' in node.text:
+                new_nodes = split_nodes_delimiter(
+                    new_nodes, '_', TextType.ITALIC)
+            if '`' in node.text:
+                new_nodes = split_nodes_delimiter(
+                    new_nodes, '`', TextType.CODE)
+
+    return new_nodes
+
+
+print(text_to_textnodes(
+    'This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)'))
+
+
+# [TextNode(This is , TextType.TEXT, None),
+# TextNode(text, TextType.BOLD, None),
+# TextNode( with an , TextType.TEXT, None),
+# TextNode(italic, TextType.ITALIC, None),
+# TextNode( word and a , TextType.TEXT, None),
+# TextNode(code block, TextType.CODE, None),
+# TextNode( and an , TextType.TEXT, None),
+# TextNode(obi wan image, TextType.IMAGE, https://i.imgur.com/fJRm4Vk.jpeg),
+# TextNode( and a , TextType.TEXT, None),
+# TextNode(link, TextType.LINK, https://boot.dev)]
