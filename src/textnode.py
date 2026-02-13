b4a1733ddd1d21443a95aeebf704d89d089a74c8
yk-david <k.yongkeon@gmail.com>
2026-02-13 14:57:41 +0900

    delete unnecessaray comment

--- src/textnode.py
+++ src/textnode.py
@@ -133,19 +133,3 @@ def text_to_textnodes(text):
                     new_nodes, '`', TextType.CODE)
 
     return new_nodes
-
-
-# print(text_to_textnodes(
-#     'This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)'))
-
-
-# [TextNode(This is , TextType.TEXT, None),
-# TextNode(text, TextType.BOLD, None),
-# TextNode( with an , TextType.TEXT, None),
-# TextNode(italic, TextType.ITALIC, None),
-# TextNode( word and a , TextType.TEXT, None),
-# TextNode(code block, TextType.CODE, None),
-# TextNode( and an , TextType.TEXT, None),
-# TextNode(obi wan image, TextType.IMAGE, https://i.imgur.com/fJRm4Vk.jpeg),
-# TextNode( and a , TextType.TEXT, None),
-# TextNode(link, TextType.LINK, https://boot.dev)]
