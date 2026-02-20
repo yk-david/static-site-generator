1c330953a0a4db08f2c785f6d0d4941b2e73018f
yk-david <k.yongkeon@gmail.com>
2026-02-20 14:19:11 +0900

    implement logic for copying files from static to public

--- src/main.py
+++ src/main.py
@@ -1,13 +1,32 @@
 from textnode import TextType, TextNode
 import os
+import shutil
 
 
 def main():
-    node = TextNode('My webpage', TextType.LINK, 'https://www.fredu.net')
-    print(node)
+    copy_static()
+
 
 def copy_static():
-    print(os.listdir)
+    if os.path.exists('public'):
+        print('deleting any existing \'public\' directory...')
+        shutil.rmtree('public')
+    print('initializing \'public\' directory...')
+    os.makedirs('public')
 
-copy_static()
-# main()
+    src = os.listdir('static')
+    print('now in static folder...', src)
+    for item in src:
+        path = os.path.join('static', item)
+        print(f'is {item} a file? checking....', os.path.isfile(path))
+        if os.path.isfile(path):
+            print(f'copying {item}...')
+            shutil.copy(path, 'public')
+        else:
+            path = os.path.join(path, item)
+
+    dst = os.listdir('public')
+    print('now in public folder...', dst)
+
+
+main()
